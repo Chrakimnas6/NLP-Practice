3e93c267f0e9651290003ac15aec69854e64c8a8
Hao Chen <hchen56@u.rochester.edu>
2019-05-06 00:47:03 +0900

    first commit

---
+++ NLP-Practice.py
@@ -0,0 +1,11 @@
+## 00. 文字列の逆順
+target = 'stressed'
+result = target[::-1]
+print('00. 文字列の逆順')
+print(result)
+
+## 01. 「パタトクカシーー」
+target = 'パタトクカシ'
+result = target[::2]
+print('01. 「パタトクカシーー」')
+print(result)
---
+++ Playground.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class Animal(object):
+    def __init__(self, species, age):
+        self.species = species
+        self.age = age
+
+    def isPerson(self):
+        return self.species == "Homo Sapiens"
+
+    def ageOneYear(self):
+        self.age += 1
+
+class Dog(Animal):
+    def ageOneYear(self):
+        self.age += 7
+
+a = Dog('bird',10)
+a.ageOneYear()
+# print(a.age)
+
+x = np.array([[6,7],[8,9]])
+
+print (np.sum(x))
