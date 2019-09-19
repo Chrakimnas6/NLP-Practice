a684b1e3b5d8d6f1ec716ea4c3b710f4220330a8
ETL <ETL@ETL.local>
2019-09-20 01:03:07 +0900

    update 13

--- NLP-Practice.py
+++ NLP-Practice.py
@@ -169,7 +169,7 @@ with open('hightemp.txt') as data_file:
 
 # expand t 1 hightemp.ext
 
-# 12. 1列目をcol1.txtに，2列目をcol2.txtに保存
+## 12. 1列目をcol1.txtに，2列目をcol2.txtに保存
 with open('hightemp.txt') as data_file, open('col1.txt', mode='w') as col1_file, open('col2.txt', mode='w') as col2_file:
     for line in data_file:
         cols = line.split('\t')
@@ -180,8 +180,15 @@ with open('hightemp.txt') as data_file, open('col1.txt', mode='w') as col1_file,
 # For compare, cut -f 1 hightemp.txt > col1_test.txt
 # diff --report-identical-files col1.txt col1_test.txt
 
+## 13. col1.txtとcol2.txtをマージ
 
+with open('col1.txt') as col1_file, open('col2.txt') as col2_file, open('merge.txt', mode='w') as write_file:
+    for col1_line, col2_line in zip(col1_file, col2_file):
+        write_file.write(col1_line.rstrip() + '\t' + col2_line.rstrip() + '\n')
 
+# Unix command
+# paste col1.txt col2.txt > merge_test.txt
+# and use diff
 
 
 
