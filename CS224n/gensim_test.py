ae185b679e6651441112c5decabbd09f9e419ba6
ETL <etc@itishinoMacBook-Air.local>
2019-10-28 17:34:07 +0900

    working on a1

---
+++ CS224n/gensim_test.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+from sklearn.decomposition import PCA
+
+from gensim.test.utils import datapath, get_tmpfile
+from gensim.models import KeyedVectors
+from gensim.scripts.glove2word2vec import glove2word2vec
+
+glove_file = datapath('/Users/ETL/Desktop/Corpora/glove.6B/glove.6B.100d.txt')
+word2vec_glove_file = get_tmpfile("glove.6B.100d.word2vec.txt")
+glove2word2vec(glove_file, word2vec_glove_file)
+
+model = KeyedVectors.load_word2vec_format(word2vec_glove_file)
+
+def analogy(x1, x2, y1):
+    result = model.most_similar(positive=[y1, x2], negative=[x1])
+    return result[0][0]
+
+# print(analogy('japan', 'japanese', 'australia'))
+
+def display_pca_scatterplot(model, words = None, sample = 0):
+    if words == None:
+        words = np.random.choice(list(model.vocab.keys()), sample)
+    else:
+        words = [word for word in model.vocab]
+
+    word_vectors = np.array([model[w] for w in words])
+
+    twodim = PCA().fit_transform(word_vectors)[:,:2]
+
+    plt.figure(figsize=(6,6))
+    plt.scatter(twodim[:,0], twodim[:,1], edgecolors='k', c='r')
+    for word, (x, y) in zip (words, twodim):
+        plt.text(x+0.05, y + 0.05, word)
+
+display_pca_scatterplot(model, 
+                        ['coffee', 'tea', 'beer', 'wine', 'brandy', 'rum', 'champagne', 'water',
+                         'spaghetti', 'borscht', 'hamburger', 'pizza', 'falafel', 'sushi', 'meatballs',
+                         'dog', 'horse', 'cat', 'monkey', 'parrot', 'koala', 'lizard',
+                         'frog', 'toad', 'monkey', 'ape', 'kangaroo', 'wombat', 'wolf',
+                         'france', 'germany', 'hungary', 'luxembourg', 'australia', 'fiji', 'china',
+                         'homework', 'assignment', 'problem', 'exam', 'test', 'class',
+                         'school', 'college', 'university', 'institute'])
+
+
