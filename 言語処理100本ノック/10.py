e1ad37d43faa4f591c1216cf419da40307df9724
ETL <etc@itishinoMacBook-Air.local>
2020-05-29 20:09:46 +0900

    update 10

--- 言語処理100本ノック/10.py
+++ 言語処理100本ノック/10.py
@@ -1,10 +1,12 @@
-## 10.　行数のカウント
+## 10.　行数のカウント update
+import pandas as pd
 count = 0
-with open('hightemp.txt') as data_file:
+with open('popular-names.txt') as data_file:
     for line in data_file:
         count += 1
 
-print(count)
+df = pd.read_csv('popular-names.txt', header=None)
+print(len(df))
 
 # Unix command 
 # wc -l 
