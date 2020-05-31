cd2f306a9b029ecbfd48e206872d930a922b2554
ETL <hchen56@u.rochester.edu>
2020-05-31 22:51:01 +0900

    update 20

--- 言語処理100本ノック/20.py
+++ 言語処理100本ノック/20.py
@@ -1,9 +1,14 @@
 # 20. JSONデータの読み込み
 
-import json
+# import json
 
-with open('jawiki-country.json') as data_file:
-    for line in data_file:
-        data_jason = json.loads(line)
-        if data_jason['title'] == 'イギリス':
-            print(data_jason['text'])
+# with open('jawiki-country.json') as data_file:
+#     for line in data_file:
+#         data_jason = json.loads(line)
+#         if data_jason['title'] == 'イギリス':
+#             print(data_jason['text'])
+
+import pandas as pd
+wiki = pd.read_json('jawiki-country.json', lines=True)
+print(wiki[wiki['title'] == 'イギリス'].text.values)
+
